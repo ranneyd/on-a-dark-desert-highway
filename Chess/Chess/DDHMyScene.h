28d9b84d8e73c360a221d68b08005152b0307ad7
ranneyd <dustinpkane@gmail.com>
2014-02-01 16:49:55 -0800

    adding project

---
+++ Chess/Chess/DDHMyScene.h
@@ -0,0 +1,13 @@
+//
+//  DDHMyScene.h
+//  Chess
+//
+
+//  Copyright (c) 2014 Dark Desert Highway Software. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface DDHMyScene : SKScene
+
+@end
