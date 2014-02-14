796391ca01c17d295418b922d60619c2c60f07fc
ranneyd <dustinpkane@gmail.com>
2014-02-14 03:01:11 -0800

    need to commit

--- Chess/DDHChess/DDHViewController.h
+++ Chess/DDHChess/DDHViewController.h
@@ -1,6 +1,6 @@
 //
-//  SHCViewController.h
-//  SHCReversiGame
+//  DDHViewController.h
+//  DDHChess
 //
 //  Created by Ray Wenderlich and modified by Dustin Kane, Will Clausen and Zakkai Davidson on 2/15/14.
 //  Copyright (c) 2012 Colin Eberhardt. All rights reserved.
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface SHCViewController : UIViewController
+@interface DDHViewController : UIViewController
 @property (weak, nonatomic) IBOutlet UIImageView *backgroundImage;
 @property (weak, nonatomic) IBOutlet UILabel *blackScore;
 @property (weak, nonatomic) IBOutlet UILabel *whiteScore;
