100516f79f3c50487d1b2222370922caddf0251a
Dustin Kane <dustinkane@Dustins-Mac.local>
2014-02-14 13:09:12 -0800

    new classes

---
+++ Chess/DDHPlayer.h
@@ -0,0 +1,13 @@
+//
+//  DDHPlayer.h
+//  Chess
+//
+//  Created by Dustin Kane on 2/14/14.
+//  Copyright (c) 2014 Dark Desert Highway Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DDHPlayer : NSObject
+
+@end
---
+++ Chess/DDHPlayer.m
@@ -0,0 +1,13 @@
+//
+//  DDHPlayer.m
+//  Chess
+//
+//  Created by Dustin Kane on 2/14/14.
+//  Copyright (c) 2014 Dark Desert Highway Software. All rights reserved.
+//
+
+#import "DDHPlayer.h"
+
+@implementation DDHPlayer
+
+@end
