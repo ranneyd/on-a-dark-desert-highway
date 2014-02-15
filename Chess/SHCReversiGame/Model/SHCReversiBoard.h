e56281b48b0473495e9c60b6522deb6196633ad5
Dustin Kane <dustinkane@Dustins-Mac.local>
2014-02-15 14:39:11 -0800

    tutorial is happening more

---
+++ Chess/SHCReversiGame/Model/SHCReversiBoard.h
@@ -0,0 +1,13 @@
+//
+//  SHCReversiBoard.h
+//  SHCReversiGame
+//
+//  Created by Dustin Kane on 2/15/14.
+//  Copyright (c) 2014 Colin Eberhardt. All rights reserved.
+//
+
+#import "SHCBoard.h"
+
+@interface SHCReversiBoard : SHCBoard
+
+@end
---
+++ Chess/SHCReversiGame/Model/SHCReversiBoard.m
@@ -0,0 +1,13 @@
+//
+//  SHCReversiBoard.m
+//  SHCReversiGame
+//
+//  Created by Dustin Kane on 2/15/14.
+//  Copyright (c) 2014 Colin Eberhardt. All rights reserved.
+//
+
+#import "SHCReversiBoard.h"
+
+@implementation SHCReversiBoard
+
+@end
