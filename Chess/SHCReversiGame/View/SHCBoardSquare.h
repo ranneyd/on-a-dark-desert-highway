6b49ac973b8cfeb4cb0d3ef07aee33bc6690c192
Dustin Kane <dustinkane@Dustins-Mac.local>
2014-02-15 14:47:54 -0800

    tutorial is happening more and more

---
+++ Chess/SHCReversiGame/View/SHCBoardSquare.h
@@ -0,0 +1,13 @@
+//
+//  SHCBoardSquare.h
+//  SHCReversiGame
+//
+//  Created by Dustin Kane on 2/15/14.
+//  Copyright (c) 2014 Colin Eberhardt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SHCBoardSquare : UIView
+
+@end
