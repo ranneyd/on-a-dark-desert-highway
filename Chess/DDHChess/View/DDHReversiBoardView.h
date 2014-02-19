fbbddd9cd7056552808503020dff15820344eaba
ranneyd <dustinpkane@gmail.com>
2014-02-18 19:45:56 -0800

    adding board view

---
+++ Chess/DDHChess/View/DDHReversiBoardView.h
@@ -0,0 +1,13 @@
+//
+//  DDHReversiBoardView.h
+//  DDHChess
+//
+//  Created by Dustin Kane on 2/18/14.
+//  Copyright (c) 2014 Dark Desert Highway Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DDHReversiBoardView : UIView
+
+@end
---
+++ Chess/DDHChess/View/DDHReversiBoardView.m
@@ -0,0 +1,31 @@
+//
+//  DDHReversiBoardView.m
+//  DDHChess
+//
+//  Created by Dustin Kane on 2/18/14.
+//  Copyright (c) 2014 Dark Desert Highway Software. All rights reserved.
+//
+
+#import "DDHReversiBoardView.h"
+
+@implementation DDHReversiBoardView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
