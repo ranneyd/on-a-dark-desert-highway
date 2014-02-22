ad75312fed30b3d1e02858da284da78ff4ca53c8
Dustin Kane <dustinkane@Dustins-Mac.local>
2014-02-21 16:53:21 -0800

    added piece class

---
+++ Chess/DDHChess/Model/DDHPiece.h
@@ -0,0 +1,13 @@
+//
+//  DDHPiece.h
+//  DDHChess
+//
+//  Created by Dustin Kane on 2/21/14.
+//  Copyright (c) 2014 Dark Desert Highway Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DDHPiece : NSObject
+
+@end
