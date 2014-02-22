b06191561103c688b6ac93313855f1981861a577
Dustin Kane <dustinkane@Dustins-Mac.local>
2014-02-21 23:42:27 -0800

    adding the null piece

---
+++ Chess/DDHChess/Model/DDHNullPiece.h
@@ -0,0 +1,13 @@
+//
+//  DDHNullPiece.h
+//  DDHChess
+//
+//  Created by Dustin Kane on 2/21/14.
+//  Copyright (c) 2014 Dark Desert Highway Software. All rights reserved.
+//
+
+#import "DDHPiece.h"
+
+@interface DDHNullPiece : DDHPiece
+
+@end
