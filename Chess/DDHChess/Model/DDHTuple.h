ccdace75b06b7decc0a390e83e7be9a6bf4f01c3
Dustin Kane <dustinkane@Dustins-Mac.local>
2014-02-27 13:49:05 -0800

    making a Tuple object

---
+++ Chess/DDHChess/Model/DDHTuple.h
@@ -0,0 +1,13 @@
+//
+//  DDHTuple.h
+//  DDHChess
+//
+//  Created by Dustin Kane on 2/27/14.
+//  Copyright (c) 2014 Dark Desert Highway Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DDHTuple : NSObject
+
+@end
---
+++ Chess/DDHChess/Model/DDHTuple.m
@@ -0,0 +1,13 @@
+//
+//  DDHTuple.m
+//  DDHChess
+//
+//  Created by Dustin Kane on 2/27/14.
+//  Copyright (c) 2014 Dark Desert Highway Software. All rights reserved.
+//
+
+#import "DDHTuple.h"
+
+@implementation DDHTuple
+
+@end
