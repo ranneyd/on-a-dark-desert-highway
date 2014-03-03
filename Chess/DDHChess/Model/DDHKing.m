db17affe4c2b00398f92cfff7c03cbd148f05ca1
Will Clausen <wclausen@hmc.edu>
2014-03-03 15:03:53 -0800

    Kings now move properly, not considering check. Every piece moves around the board in a simplified version of how they are supposed to.

--- Chess/DDHChess/Model/DDHKing.m
+++ Chess/DDHChess/Model/DDHKing.m
@@ -16,41 +16,44 @@
     NSUInteger x = [self x];
     NSUInteger y = [self y];
     
-    // Store moves in here.
-    DDHTuple* points[8];
+    // Store move types in here.
+    NSMutableArray* points = [[NSMutableArray alloc] initWithCapacity:8];
     
     // up
-    [points[0] setX: 0];
-    [points[0] setY: -1];
-    // up right
-    [points[1] setX:  1];
-    [points[1] setY: -1];
-    // right
-    [points[2] setX:  1];
-    [points[2] setY:  0];
-    // down right
-    [points[3] setX:  1];
-    [points[3] setY:  1];
+    [points addObject:[[DDHTuple alloc] initWithX:0 andY:-1]];
+    
     // down
-    [points[4] setX:  0];
-    [points[4] setY:  1];
-    // down left
-    [points[5] setX: -1];
-    [points[5] setY:  1];
+    [points addObject:[[DDHTuple alloc] initWithX:0 andY:1]];
+    
     // left
-    [points[6] setX: -1];
-    [points[6] setY:  0];
-    // up left
-    [points[7] setX: -1];
-    [points[7] setY: -1];
+    [points addObject:[[DDHTuple alloc] initWithX:-1 andY:0]];
+    
+    // right
+    [points addObject:[[DDHTuple alloc] initWithX:1 andY:0]];
+    
+    // up 1 left 1
+    [points addObject:[[DDHTuple alloc] initWithX:-1 andY:-1]];
+    
+    // down 1 left 1
+    [points addObject:[[DDHTuple alloc] initWithX:-1 andY:1]];
+    
+    // up 1 right 1
+    [points addObject:[[DDHTuple alloc] initWithX:1 andY:-1]];
+    
+    // down 1 right 1
+    [points addObject:[[DDHTuple alloc] initWithX:1 andY:1]];
     
     NSMutableArray *highlighting = [[NSMutableArray alloc]init];
     
     for(int i = 0; i < 8; i++){
-        int dx = x + [points[i] x];
-        int dy = y + [points[i] y];
-        if([self onBoard:board AtColumn:dx andRow:dy])
-            [highlighting addObject:[[DDHTuple alloc] initWithX:dx andY:dy]];
+        DDHTuple* nextMove = [points objectAtIndex:i];
+        int dx = x + [nextMove x];
+        int dy = y + [nextMove y];
+        if([self onBoard:board AtColumn:dx andRow:dy]) {
+            if ([board pieceAtColumn:dx andRow:dy notBelongingToPlayer:[self getPlayer]]) {
+                [highlighting addObject:[[DDHTuple alloc] initWithX:dx andY:dy]];
+            }
+        }
     }
     return highlighting;
 }
