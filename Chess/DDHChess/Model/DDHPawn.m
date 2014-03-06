8c81ff81227453b689abe02752fa57671e8a3c83
Will Clausen <wclausen@hmc.edu>
2014-03-06 13:48:47 -0800

    No more teleporting pawns!

--- Chess/DDHChess/Model/DDHPawn.m
+++ Chess/DDHChess/Model/DDHPawn.m
@@ -18,7 +18,8 @@
 -(id) initWithPlayer:(ChessPlayer) player atColumn:(NSUInteger)column andRow:(NSUInteger)row
 {
     self = [super initWithPlayer:player atColumn:column andRow:row];
-    hasNotMoved = YES;
+    hasNotMoved = YES
+    ;
     return self;
 }
 
@@ -30,31 +31,45 @@
     NSInteger column = [self x];
     NSInteger row = [self y];
     
+    NSInteger verticalMove = row + pow(-1, [self getPlayer]);
+    
+    // Variables for attacking other pieces. If the pawn can't move left or right,
+    // set the values to be off the board!
+    NSInteger leftMove = column - 1 > 0 ? column - 1 : -1;
+    NSInteger rightMove = column + 1 < [board getColumns] ? column + 1 : -1;
+    
+    // Now, to determining moves. First things first, if the pawn is pinned, it can't move.
+    // TODO: Implement isPinned!
+//    if ([self isPinned])
+//    {
+//        return highlighting;
+//    }
+    
+    // Next check for ability to move two squares forward.
     if (hasNotMoved) {
-        // Make pawn highlight two squares in front...
+        // Make pawn highlight two squares in front...note the direction of "front" is based on the player
         NSInteger verticalDoubleMove = row + 2*pow(-1, [self getPlayer]);
         
+        // Sanity check to make sure the move is on the board
         if ([self onBoard:board AtColumn:column andRow:verticalDoubleMove]) {
-            if ([board isEmptySquareAtColumn:column andRow:verticalDoubleMove]) {
+            
+            // Make sure both the square immediately in front of and two squares in front of the pawn are empty.
+            if ([board isEmptySquareAtColumn:column andRow:verticalDoubleMove] && [board isEmptySquareAtColumn:column andRow:verticalMove]) {
                 [highlighting addObject:[[DDHTuple alloc] initWithX:column andY:verticalDoubleMove]];
             }
         }
         
     }
     
-    NSInteger verticalMove = row + pow(-1, [self getPlayer]);
-    
     if ([self onBoard:board AtColumn:column andRow:verticalMove]) {
+        
+        // If the square in front of the pawn is empty, it can move there.
         if ([board isEmptySquareAtColumn:column andRow:verticalMove]) {
             [highlighting addObject:[[DDHTuple alloc] initWithX:column andY:verticalMove]];
         }
     }
     
-    // Variables for attacking other pieces.
-    NSInteger leftMove = column - 1 > 0 ? column - 1 : column;
-    NSInteger rightMove = column + 1 < [board getColumns] ? column + 1 : column;
-    
-    // Check if we can attack other pieces.
+    // Check if we can attack other pieces to the left.
     if ([self onBoard:board AtColumn:leftMove andRow:verticalMove]) {
         if (![board isEmptySquareAtColumn:leftMove andRow:verticalMove]) {
             if ([board pieceAtColumn:leftMove andRow:verticalMove notBelongingToPlayer:[self getPlayer]]) {
@@ -63,6 +78,7 @@
         }
     }
     
+    // Check if the pawn can attack other pieces to the right.
     if ([self onBoard:board AtColumn:rightMove andRow:verticalMove]) {
         if (![board isEmptySquareAtColumn:rightMove andRow:verticalMove]) {
             if ([board pieceAtColumn:rightMove andRow:verticalMove notBelongingToPlayer:[self getPlayer]]) {
