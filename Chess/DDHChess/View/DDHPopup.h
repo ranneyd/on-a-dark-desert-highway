5a48506bf9f87beb33a8119e8f98e380e7203dd7
Dustin Kane <dustinkane@Dustins-Mac.local>
2014-04-26 18:58:33 -0700

    Added DDHPopup

---
+++ Chess/DDHChess/View/DDHPopup.h
@@ -0,0 +1,16 @@
+//
+//  DDHPopup.h
+//  Chess?
+//
+//  Created by Dustin Kane on 4/26/14.
+//  Copyright (c) 2014 Dark Desert Highway Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DDHPopup : UIView
+
+- (id)initWithFrame:(CGRect)frame andText:(NSString *) text;
+
+
+@end
---
+++ Chess/DDHChess/View/DDHPopup.m
@@ -0,0 +1,31 @@
+//
+//  DDHPopup.m
+//  Chess?
+//
+//  Created by Dustin Kane on 4/26/14.
+//  Copyright (c) 2014 Dark Desert Highway Software. All rights reserved.
+//
+
+#import "DDHPopup.h"
+
+@implementation DDHPopup
+
+- (id)initWithFrame:(CGRect)frame andText:(NSString *) text
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
